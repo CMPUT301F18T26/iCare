49569e14957eba28124cc2569f6295a269baa74a
conormcquitty <conormcquitty@hotmail.com>
2018-10-30 21:59:06 -0600

    Added Record class and added RecordTest JUnit tests.

---
+++ app/src/main/java/com/example/cmput301f18t26/icare/Record.java
@@ -0,0 +1,30 @@
+package com.example.cmput301f18t26.icare;
+
+import java.util.Calendar;
+
+public class Record {
+
+    private String title;
+    private Calendar date;
+    private String comment;
+
+
+    public Record(String title, Calendar date, String comment){
+        this.title = title;
+        this.date = date;
+        this.comment = comment;
+    }
+
+    public String getTitle(){return this.title;}
+
+    public void setTitle(String title) {this.title = title;}
+
+    public Calendar getDate() {return this.date;}
+
+    public void setDate(Calendar date) {this.date = date;}
+
+    public String getComment() {return this.comment;}
+
+    public void setComment(String comment) {this.comment = comment;}
+
+}
