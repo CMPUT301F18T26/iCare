1cbb9732d8b37b5c27290c710e89f5e992d84db8
Amrinder Grewal <asgrewal@ualberta.ca>
2018-11-09 16:51:42 -0700

    Profile stuff done

--- app/src/main/java/com/example/cmput301f18t26/icare/MainActivity.java
+++ app/src/main/java/com/example/cmput301f18t26/icare/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_care_provider_condition_view);
+        setContentView(R.layout.activity_edit_your_profile);
     }
 }
