d52524e157d6407e44f17e117490b11241b89cbf
Amrinder Grewal <asgrewal@ualberta.ca>
2018-11-09 15:35:02 -0700

    UI: View patients assigned to a doctor done

--- app/src/main/java/com/example/cmput301f18t26/icare/MainActivity.java
+++ app/src/main/java/com/example/cmput301f18t26/icare/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_add_or_edit_condition);
+        setContentView(R.layout.activity_signup);
     }
 }
