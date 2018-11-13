389bd3c779b446ee5b52bc4766b5be6820245cd6
tqian1 <t.qian7@gmail.com>
2018-11-12 17:38:16 -0700

    improvements to datacontroller

--- app/src/main/java/com/example/cmput301f18t26/icare/Controllers/DataController.java
+++ app/src/main/java/com/example/cmput301f18t26/icare/Controllers/DataController.java
@@ -5,31 +5,39 @@ import com.example.cmput301f18t26.icare.Problem;
 import com.example.cmput301f18t26.icare.Record;
 import com.example.cmput301f18t26.icare.CareProvider;
 import com.example.cmput301f18t26.icare.User;
+import com.searchly.jestdroid.JestDroidClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * DataController is a Singleton class used for accessing ElasticSearch data associated with
+ * DataController is a Singleton class used for working with ElasticSearch data associated with
  * our app. An instance of it may be retrieved using DataController.getInstance();
  */
 class DataController {
 
-    // URL our Elasticsearch is hosted at, and the lone instance of our DataController
+    // The lone instance of our DataController
     private static DataController onlyInstance = null;
-    private final String ElasticSearchURL = "http://cmput301.softwareprocess.es:8080/cmput301f18t26test";
+    private final String elasticSearchURL = "http://cmput301.softwareprocess.es:8080/cmput301f18t26";
+    private JestDroidClient jestClient;
+
     private User currentUser = null;
     private List<User> userList = new ArrayList<>();
 
 
     // We use a private constructor here to enforce Singleton Pattern
     private DataController() {
+        jestClient = new JestDroidClient();
         fetch();
     }
 
+    /**
+     * Below are the public methods that should be used for interacting with data controller
+     */
+
     // Use this method to access our DataController Instance
     public static DataController getInstance() {
-        // Lazy load it, WHY NOT
+        // Lazy load it
         if (onlyInstance == null) {
             onlyInstance = new DataController();
         }
@@ -37,6 +45,7 @@ class DataController {
         return onlyInstance;
     }
 
+    // Fetch problems, records associated with currentUser from ElasticSearch to our cache
     public void fetch() {
     }
 
