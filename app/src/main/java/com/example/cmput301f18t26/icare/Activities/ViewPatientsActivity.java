b1b3be850c4587276d76f4c5cbf0a9e3429b7b9e
Amrinder Grewal <asgrewal@ualberta.ca>
2018-11-18 16:31:59 -0700

    Options menu loading added in ViewPatientsActivity

--- app/src/main/java/com/example/cmput301f18t26/icare/Activities/ViewPatientsActivity.java
+++ app/src/main/java/com/example/cmput301f18t26/icare/Activities/ViewPatientsActivity.java
@@ -1,8 +1,12 @@
 package com.example.cmput301f18t26.icare.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,11 +31,38 @@ public class ViewPatientsActivity extends AppCompatActivity {
 
         dataController = DataController.getInstance();
 
-        patientList = findViewById(R.id.care_provider_patient_list);
-        addPatientButton = findViewById(R.id.add_new_patient);
+//        patientList = findViewById(R.id.care_provider_patient_list);
+//        addPatientButton = findViewById(R.id.add_new_patient);
 
 //        patientListAdapter = new ArrayAdapter<User>(
 //                this, R.layout.activity_care_provider_patients_list_item, dataController.getPatients()
 //        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Creating the menu options from the xml file
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.care_provider_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        // Getting the id of the menu item selected
+        int id = item.getItemId();
+        // Executing code depending on which item is selected
+        switch (id){
+            case R.id.contact_information:
+                // Creating the intent
+                intent = new Intent(ViewPatientsActivity.this, ViewProfileActivity.class);
+                // Passing in the user id that will have its information displayed
+                intent.putExtra("user_id", this.dataController.getCurrentUser().getUID());
+                // Launching the intent
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
