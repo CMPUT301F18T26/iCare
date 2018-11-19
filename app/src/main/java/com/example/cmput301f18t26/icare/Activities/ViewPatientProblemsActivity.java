7d73fac40fe4c0f096c69653a1c61709b36eb903
zdrever <zdrever@ualberta.ca>
2018-11-18 22:57:43 -0700

    add missing file

---
+++ app/src/main/java/com/example/cmput301f18t26/icare/Activities/ViewPatientProblemsActivity.java
@@ -0,0 +1,15 @@
+package com.example.cmput301f18t26.icare.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cmput301f18t26.icare.R;
+
+public class ViewPatientProblemsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_care_provider_problem_view);
+    }
+}
