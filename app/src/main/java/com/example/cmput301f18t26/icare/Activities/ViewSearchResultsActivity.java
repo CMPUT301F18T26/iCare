afabfa1fb8062600df20ce13f5dcd9e677a7ee52
zdrever <zdrever@ualberta.ca>
2018-11-28 14:46:39 -0700

    Make search items viewable

--- app/src/main/java/com/example/cmput301f18t26/icare/Activities/ViewSearchResultsActivity.java
+++ app/src/main/java/com/example/cmput301f18t26/icare/Activities/ViewSearchResultsActivity.java
@@ -1,21 +1,20 @@
 package com.example.cmput301f18t26.icare.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Switch;
-import android.widget.ToggleButton;
 
 import com.example.cmput301f18t26.icare.Controllers.DataController;
 import com.example.cmput301f18t26.icare.Models.BaseRecord;
 import com.example.cmput301f18t26.icare.Models.Problem;
 import com.example.cmput301f18t26.icare.R;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ViewSearchResultsActivity extends AppCompatActivity {
@@ -39,6 +38,41 @@ public class ViewSearchResultsActivity extends AppCompatActivity {
 
         dataController = DataController.getInstance();
 
+        results = findViewById(R.id.search_results);
+        toggleResults = findViewById(R.id.toggle_results);
+
+        toggleResults.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    results.setAdapter(recordAdapter);
+                } else {
+                    // The toggle is disabled
+                    results.setAdapter(problemAdapter);
+                }
+            }
+        });
+
+        results.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+                if (results.getAdapter() == recordAdapter) {
+                    dataController.setSelectedRecord(recordList.get(position));
+                    intent = new Intent(getApplicationContext(), ViewRecordActivity.class);
+                } else {
+                    dataController.setSelectedProblem(problemList.get(position));
+                    intent = new Intent(getApplicationContext(), PatientViewProblemActivity.class);
+                }
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
         recordList = dataController.getRecordSearchResults();
         problemList = dataController.getProblemSearchResults();
 
@@ -55,20 +89,15 @@ public class ViewSearchResultsActivity extends AppCompatActivity {
                 problemList
         );
 
-        results = findViewById(R.id.search_results);
         results.setAdapter(problemAdapter);
-        toggleResults = findViewById(R.id.toggle_results);
+    }
 
-        toggleResults.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    results.setAdapter(recordAdapter);
-                } else {
-                    // The toggle is disabled
-                    results.setAdapter(problemAdapter);
-                }
-            }
-        });
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        recordList = dataController.getRecordSearchResults();
+        problemList = dataController.getProblemSearchResults();
+        results.setAdapter(problemAdapter);
     }
 }
