e1bcaa1dd9e3c52153e513767a4505fe36963247
tsantos301 <tsantos@ualberta.ca>
2018-11-27 20:15:54 -0700

    Current Location working

--- app/src/main/java/com/example/cmput301f18t26/icare/Activities/AddEditRecordActivity.java
+++ app/src/main/java/com/example/cmput301f18t26/icare/Activities/AddEditRecordActivity.java
@@ -1,9 +1,14 @@
 package com.example.cmput301f18t26.icare.Activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -15,7 +20,6 @@ import com.example.cmput301f18t26.icare.Controllers.DataController;
 import com.example.cmput301f18t26.icare.Models.Problem;
 import com.example.cmput301f18t26.icare.R;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
@@ -23,10 +27,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+
 
 import static android.support.test.InstrumentationRegistry.getContext;
 
-public class AddEditRecordActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
+public class AddEditRecordActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
     private DataController dataController;
     private EditText titleEntry;
     private EditText descriptionEntry;
@@ -36,6 +43,13 @@ public class AddEditRecordActivity extends AppCompatActivity implements BottomNa
     private Fragment infoFragment;
     private Fragment geoFragment = new GeolocationFragment();
     private Fragment bodyFragment = new BodylocationFragment();
+    private boolean mLocationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location lastKnownLocation;
+    private GoogleMap map;
+    private static final int DEFAULT_ZOOM = 14;
+    private LatLng defaultLocation = new LatLng(53.509,-113.5);
 
     SupportMapFragment sMapFragment;
 
@@ -45,7 +59,7 @@ public class AddEditRecordActivity extends AppCompatActivity implements BottomNa
 
         //Create a new instance of the support map fragment
         sMapFragment = SupportMapFragment.newInstance();
-        
+
 
         Bundle extras = getIntent().getExtras();
         dataController = DataController.getInstance();
@@ -63,11 +77,11 @@ public class AddEditRecordActivity extends AppCompatActivity implements BottomNa
     }
 
 
-    private boolean loadFragment(Fragment fragment){
-        if (fragment != null){
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container,fragment)
+                    .replace(R.id.fragment_container, fragment)
                     .commit();
             return true;
         }
@@ -81,7 +95,7 @@ public class AddEditRecordActivity extends AppCompatActivity implements BottomNa
         Fragment fragment = null;
         android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
 
-        switch(menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.info:
                 fragment = infoFragment;
                 break;
@@ -111,19 +125,65 @@ public class AddEditRecordActivity extends AppCompatActivity implements BottomNa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         //when map is loaded do everything in here.
-
+        map = googleMap;
         //MapsInitializer.initialize(getContext());
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+        }
 
         // Add a marker in Edmonton, and move the camera to the correct position.
         LatLng edmonton = new LatLng(53.5444, -113.4909);
         //Create the marker
-        googleMap.addMarker(new MarkerOptions().position(edmonton).title("Edmonton Alberta"));
+        map.addMarker(new MarkerOptions().position(edmonton).title("Edmonton Alberta"));
         //Set the initial camera zoom level
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        map.moveCamera(CameraUpdateFactory.zoomTo(10));
         //Move the camera to the marker position
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(edmonton));
+        map.moveCamera(CameraUpdateFactory.newLatLng(edmonton));
 
+    }
+
+
+    public void getCurrentLocation(){
+        try {
+            if (mLocationPermissionGranted) {
+                com.google.android.gms.tasks.Task<Location> locationResult = mFusedLocationClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<Location> task) {
+                        lastKnownLocation = task.getResult();
+                        if (task.isSuccessful()&&lastKnownLocation!=null) {
+
+                            // Set the map's camera position to the current location of the devices
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(lastKnownLocation.getLatitude(),
+                                            lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+
+                        } else {
+                            map.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+            else{
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation,DEFAULT_ZOOM));
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
